6bf27ab78a4286c2416597d655e2cd1da7dcdf78
asafkurbegovic <asafkurbegovic@gmail.com>
2020-07-05 13:31:48 +0200

    Game over screen update

--- components/screens/GameOver.js
+++ components/screens/GameOver.js
@@ -1,25 +1,37 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
+import Card from "../Card";
 
 const GameOver = (props) => {
   return (
-    <View style={style.screen}>
-      <Text style={style.textstyle}>Game oveeeer bitch!</Text>
+    <View style={style.screen}><Card>
+      <Text style={style.textstyle}>GAME OVER!</Text>
       <Text style={style.textstyle}>Number of rounds that took me to guess youe number is:</Text>
-      <Text>{props.rounds}</Text>
+      </Card>
+      <Card  style={style.cardStyle}><Text  style={style.textstyle}>{props.rounds}</Text></Card>
       <Button title="new game" onPress={props.onRestart} />
+    
     </View>
+
   );
 };
 
 const style = StyleSheet.create({
   screen: {
+    padding:20,
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
   textstyle:{
-    fontFamily:'niceone',
+    fontSize:36,
+    alignItems:'center'
+    //fontFamily:'niceone',
+  },
+  cardStyle:{
+    fontSize:36,
+    borderColor:'red',
+    borderWidth:2
   }
 });
 
